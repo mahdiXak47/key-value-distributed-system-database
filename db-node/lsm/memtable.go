aa11c3699fb2b8b008f120929be4fd73e63518a6
AidaJalali <aidajalaliii@gmail.com>
2025-05-18 16:15:58 +0330

    Implement merge logic (When a level is full, its layers are merged)

--- db-node/lsm/memtable.go
+++ db-node/lsm/memtable.go
@@ -39,3 +39,16 @@ func (mt *MemTable) Size() int {
 	defer mt.mu.RUnlock()
 	return len(mt.data)
 }
+
+// GetAll returns a copy of all key-value pairs in the MemTable
+func (mt *MemTable) GetAll() map[string]string {
+	mt.mu.RLock()
+	defer mt.mu.RUnlock()
+
+	// Create a copy of the data
+	copy := make(map[string]string, len(mt.data))
+	for k, v := range mt.data {
+		copy[k] = v
+	}
+	return copy
+}
